ae94bf19094d4bff70a353a2e6e3b09418b77185
Zaid Crouch <zaid.crouch@gmail.com>
2021-03-28 22:41:28 +1100

    Move Router inside App component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import MarketingPage from './MarketingPage/MarketingPage';
 import SearchPage from './SearchPage/SearchPage';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/search/">
-        <SearchPage />
-      </Route>
-      <Route path="/" exact>
-        <MarketingPage />
-      </Route>
-      <Route>404</Route>
-    </Switch>
+    <Router>
+      <Switch>
+        <Route path="/search/">
+          <SearchPage />
+        </Route>
+        <Route path="/" exact>
+          <MarketingPage />
+        </Route>
+        <Route>404</Route>
+      </Switch>
+    </Router>
   );
 }
 
