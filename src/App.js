505677cacc68462944e26ad7a690ff7a6536a6a1
Zaid Crouch <zaid.crouch@gmail.com>
2021-04-04 17:39:22 +1000

    Create app skeleton (#1)
    * Specify node version
    
    * Add prettier config file
    
    * Add Bitter and Montserrat fonts
    
    * Add normalize.css as dependency
    
    * Add react-router-dom as dependency
    
    * Add styled-components as dependency
    
    * Add global styles
    
    * Add routing structure
    
    * Add directories and placeholder components for each page
    
    * Update page title and description
    
    * Update favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import MarketingPage from './MarketingPage/MarketingPage';
+import SearchPage from './SearchPage/SearchPage';
+
+import './theme.css';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/search/">
+          <SearchPage />
+        </Route>
+        <Route path="/" exact>
+          <MarketingPage />
+        </Route>
+        <Route>404</Route>
+      </Switch>
+    </Router>
   );
 }
 
